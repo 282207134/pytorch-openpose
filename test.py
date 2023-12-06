c7707db7b7b1b6fecad5167f3242128f9a20471d
liuyu <liu282207134@yahoo.co.jp>
2023-12-06 17:26:09 +0900

    Merge remote-tracking branch 'origin/master'

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
 import torch
-
 from demo_video import input_fps
-
 if torch.cuda.is_available():
     print(f"Torch device: {torch.cuda.get_device_name()}")
 else:
     print("CUDA is not available. Check your PyTorch installation.")
-
 print("是否可用：", torch.cuda.is_available())        # 查看GPU是否可用
 print("GPU数量：", torch.cuda.device_count())        # 查看GPU数量
 print("torch方法查看CUDA版本：", torch.version.cuda)  # torch方法查看CUDA版本
